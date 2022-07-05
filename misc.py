357db23e20f8ce21eaae62293a044e2ecec21da3
ipelle <pellet.ian1@gmail.com>
2022-07-05 10:07:40 +0200

    added lab2mat function

--- misc.py
+++ misc.py
@@ -8,17 +8,18 @@ def getAll_mm_def(mm_path = './ModuleMatrix/'):
     Parameters
     ----------
     mm_path : string
-              Path to the directory containing all the membership matrices.
+        Path to the directory containing all the membership matrices.
+        
     Returns
     -------
     all_mm : pandas.DataFrame, shape=[n_mm, 5]
-             Data frame with each row cotaining information about one cluster
-             solution found in `mm_path`. The fields are : 
-                 - `Alg` : the algorithm used,
-                 - `Dataset` : the data set used,
-                 - `Param` : parameters values for the algorithm,
-                 - `Prefix` : file prefix as `Alg_Dataset_Param`,
-                 - `File` : path to the membership matrix file.
+        Data frame with each row cotaining information about one cluster solution
+        found in `mm_path`. The fields are : 
+            - `Alg` : the algorithm used,
+            - `Dataset` : the data set used,
+            - `Param` : parameters values for the algorithm,
+            - `Prefix` : file prefix as `Alg_Dataset_Param`,
+            - `File` : path to the membership matrix file.
     """
     mm_file_names = []
     #dir_in_mm = []
@@ -46,3 +47,25 @@ def getAll_mm_def(mm_path = './ModuleMatrix/'):
                            [os.path.join(mm_path,f) for f in mm_file_names]], 
                           index=['Alg', 'Dataset', 'Param', 'Prefix', 'File']).T
     return all_mm
+
+def lab2mat(lab, genes):
+    """Create membership matrix from label list.
+    
+    Parameters
+    ----------
+    lab : int array, shape=(n_samples,)
+        List of labels for each sample in the dataset.
+        
+    genes : string array, shape=(n_samples,)
+        List of gene names to use as membership matrix index.
+        
+    Returns
+    -------
+    mm : pandas.DataFrame, shape=(n_samples, n_clusters)
+        Membership matrix with samples (genes) in row and clusters in column.
+    """
+    cl = np.unique(lab)
+    mm = pd.DataFrame(0, columns=cl, index=genes)
+    for i in range(len(lab)):
+        mm.loc[genes[i],lab[i]] = 1
+    return mm
