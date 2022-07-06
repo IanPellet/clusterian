efd54a1abf85b687b717a7812e4404fdc982e7d3
ipelle <pellet.ian1@gmail.com>
2022-07-06 10:22:45 +0200

    add get_mm_def and modified getAll_mm_def accordingly

--- misc.py
+++ misc.py
@@ -1,7 +1,24 @@
 import pandas as pd
 import numpy as np
 import os
+from statistics import mean, median
 
+def get_mm_def(mm_file):
+    filename = mm_file.split('/')[-1]
+    attr = filename.split('_')
+    
+    mm_def_list = [attr[0], attr[1]]
+
+    if len(attr)>2:
+        mm_def_list.append('_'.join(attr[2:])[:-4])
+    else:
+        mm_def_list.append(None)
+    mm_def_list.append(filename[:-4])
+    mm_def_list.append(mm_file)
+
+    mm_def = pd.DataFrame(mm_def_list, index=['Alg', 'Dataset', 'Param', 'Prefix', 'File']).T
+    return mm_def
+    
 def getAll_mm_def(mm_path = './ModuleMatrix/'):
     """Get informations about membership matrices in `mm_path`.
     
@@ -12,7 +29,7 @@ def getAll_mm_def(mm_path = './ModuleMatrix/'):
         
     Returns
     -------
-    all_mm : pandas.DataFrame, shape=[n_mm, 5]
+    all_mm_def : pandas.DataFrame, shape=[n_mm, 5]
         Data frame with each row cotaining information about one cluster solution
         found in `mm_path`. The fields are : 
             - `Alg` : the algorithm used,
@@ -27,26 +44,16 @@ def getAll_mm_def(mm_path = './ModuleMatrix/'):
         mm_file_names.extend(filenames)
         #dir_in_mm.extend(dirnames)
         break
+    mm_files = [os.path.join(mm_path,f) for f in mm_file_names]
+    
+    all_mm_def_list = []
+    for i in range(len(mm_files)):
+        i_def = get_mm_def(mm_files[i])
+        i_def.index = [i]
+        all_mm_def_list.append(i_def)
 
-    algo = []   
-    dataset = []
-    parm = []
-    prefix = []
-    for filename in mm_file_names:
-        #path = os.path.join(mm_path,filename)
-        attr = filename.split('_')
-        algo.append(attr[0])
-        dataset.append(attr[1])
-        if len(attr)>2:
-            parm.append('_'.join(attr[2:])[:-4])
-        else:
-            parm.append(None)
-        prefix.append(filename[:-4])
-
-    all_mm = pd.DataFrame([algo, dataset, parm, prefix, 
-                           [os.path.join(mm_path,f) for f in mm_file_names]], 
-                          index=['Alg', 'Dataset', 'Param', 'Prefix', 'File']).T
-    return all_mm
+    all_mm_def = pd.concat(all_mm_def_list)
+    return all_mm_def
 
 def lab2mat(lab, genes):
     """Create membership matrix from label list.
@@ -69,3 +76,53 @@ def lab2mat(lab, genes):
     for i in range(len(lab)):
         mm.loc[genes[i],lab[i]] = 1
     return mm
+
+def summary(mm=None, file_name=None, method=None):
+    if mm==None and file_name==None:
+        raise('At least one of `mm` or `file_name` must be specified')
+        
+    if file_name!=None:
+        mm = pd.read_csv(file_name, header=None, index_col=0)
+        if type(mm.index[0])!=str or len(mm.index[0])<2:
+            mm = pd.read_csv(file_name, header=0, index_col=0)
+            
+    n_clusters = mm.shape[1]
+    cl_sizes = np.sum(mm, axis=0)
+    max_cl_size = int(max(cl_sizes))
+    min_cl_size = int(min(cl_sizes))
+    mean_cl_size = mean(cl_sizes)
+    med_cl_size = median(cl_sizes)
+    n_empty_cl = int(sum(cl_sizes==0))
+    n_1_cl = int(sum(cl_sizes==1))
+    
+    n_genes = mm.shape[0]
+    cl_per_genes = np.sum(mm, axis=1)
+    max_cl = int(max(cl_per_genes))
+    min_cl = int(min(cl_per_genes))
+    mean_cl = mean(cl_per_genes)
+    med_cl = median(cl_per_genes)
+    uncl_part = sum(cl_per_genes==0)/n_genes
+    in1cl_part = sum(cl_per_genes==1)/n_genes
+    
+    
+    ppt = [n_clusters, max_cl_size, min_cl_size, mean_cl_size, med_cl_size, 
+           n_empty_cl, n_1_cl, n_genes, max_cl, min_cl, mean_cl, med_cl, uncl_part,
+           in1cl_part]
+    names = ['n_clusters', 'max_cl_size', 'min_cl_size', 'mean_cl_size', 
+             'med_cl_size', 'n_empty_cl', 'n_1_cl', 'n_genes', 'max_cl', 'min_cl',
+             'mean_cl', 'med_cl', 'uncl_part', 'in1cl_part']
+    return pd.DataFrame(ppt, index=names).T
+
+def summary_all(mm_path='./MembMatrix'):
+    all_def = getAll_mm_def(mm_path)
+    all_sum_list = []
+    fname_col = all_def.columns[-1]
+    for i in all_def.index:
+        i_sum = summary(file_name=all_def.loc[i,fname_col])
+        i_sum.index = [i]
+        all_sum_list.append(i_sum)
+    return all_sum_list
+    all_sum_ppt = pd.concat(all_sum_list)
+    all_summary = pd.concat([all_def,all_sum_ppt], axis=1)
+    return all_summary
+        
